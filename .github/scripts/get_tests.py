b211b6cba6320d5cb0d7f62a4487a2b94053ff56
Oscar Barrios <obarrios@suse.com>
2025-02-03 19:01:14 +0100

    Refactor acceptance_tests_fast

--- .github/scripts/get_tests.py
+++ .github/scripts/get_tests.py
@@ -1,25 +1,12 @@
-import os.path
 import re
 import sys
 import yaml
 
 if len(sys.argv) != 2:
-    print("Expecting the xml file as argument")
-    exit(-1)
-if not os.path.isfile(sys.argv[1]):
-    print("Expecting the xml file as argument")
+    print("Expecting the XML string as argument")
     exit(-1)
 
-file_path = sys.argv[1]
-
-with open(file_path, 'r') as file:
-    result = file.read()
-pattern = re.compile(r'<li>[a-z|_]*</li>')
-match  = pattern.findall(result)
-tests = []
-for i in match:
-    i = i.replace("<li>","")
-    i = i.replace("</li>","")
-    tests.append(i)
-
-print(yaml.dump(tests))
+xml_string = sys.argv[1]
+pattern = re.compile(r'<li>([\w\-]+)</li>')
+matches = pattern.findall(xml_string)
+print(yaml.dump(matches))
