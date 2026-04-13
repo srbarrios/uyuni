4c5d34b2a7cf6eef9fadd29b1294b69bf3d4c85a
Ondrej Holecek <oholecek@suse.com>
2026-04-13 15:35:53 +0200

    Use /var/spacewalk based config flag mark

--- containers/server-image/root/usr/lib/entrypoint-lib.sh
+++ containers/server-image/root/usr/lib/entrypoint-lib.sh
@@ -5,13 +5,18 @@
 
 set -eu
 
-MANAGER_COMPLETE="/root/.MANAGER_SETUP_COMPLETE"
+MANAGER_COMPLETE_LEGACY="/root/.MANAGER_SETUP_COMPLETE"
+MANAGER_COMPLETE="/var/spacewalk/.MANAGER_SETUP_COMPLETE"
 
 check_current_installation() {
+    if [ -e "${MANAGER_COMPLETE_LEGACY}" ]; then
+        mv "$MANAGER_COMPLETE_LEGACY" "$MANAGER_COMPLETE"
+    fi
     if [ -e "${MANAGER_COMPLETE}" ]; then
         echo "Server appears to be already configured. Installation options may be ignored."
         exit 0
     fi
+
 }
 
 mark_installation_complete() {
